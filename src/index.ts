6a9f5de7a304443a2db4a240408b0ca8268bccab
Albert Maharjan <51773128+albertmaharjan94@users.noreply.github.com>
2025-11-20 22:10:33 +0545

    Replace express.json() with body-parser middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
+
 
 // In-memory storage - 
 let users: any[] = [
